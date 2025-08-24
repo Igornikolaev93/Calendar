e095ddb63f5a4547055e1a721ca1aa9ed833d505
Igornikolaev93 <goscha.nikolae2015@yandex.ru>
2025-08-24 21:00:34 +0300

    Add files via upload
    add

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction' 
+import ruLocale from '@fullcalendar/core/locales/ru';
+import esLocale from '@fullcalendar/core/locales/es';
+
+export default {
+  components: {
+    FullCalendar 
+  },
+
+
+  data: function() {
+    return {
+    calendarOptions: {  
+    plugins: [ dayGridPlugin, interactionPlugin ],  
+    initialView: 'dayGridMonth',  
+    dateClick: this.getDate,
+    weekends: true,
+    locale: ruLocale,
+    events:  [
+          { title: 'Задача 1', start: '2025-08-01' },
+          { title: 'Задача 2', start: '2025-08-20' }
+        ]
+}  
+  }  
+  },
+ methods: {
+  getDate: function(arg){
+  this.calendarOptions.events.push({title:`${arg.dateStr}`,start:new Date(`${arg.dateStr}`)})
+  },
+  changeLanguage: function() {
+    return this.calendarOptions.locale =!this.calendarOptions.locale
+
+  }
+   }}
+ 
+</script>
+
+<template >
+   <button @click="changeLanguage">Change Language</button>
+    
+ <FullCalendar :options='calendarOptions' />
+
+</template>
+
+<style >
+
+
+</style>
